e13077a2a6687b32c75710576b91791250cf4402
dimasfdhla <dimasfadhilah02@gmail.com>
2022-09-24 00:30:42 +0700

    Push Project

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+
+	<h4>Nama</h4>
+	<ul type="disc">
+		<li>Nama Depan : Dimas</li>
+		<li>Nama Belakang : Fadhilah</li>
+	</ul>
+	<br>
+	<h4>Alamat</h4>
+	<ul type="none">
+		<li> Jln. H. Naih No. 54</li>
+	</ul>
+	<h4>Tempat Lahir</h4>
+	<ul type="none">
+		<li>Semarang</li>
+	</ul>
+	<h4>Olah Raga Favorit</h4>
+	<ul type="square">
+		<li>Slebew</li>
+		<li>Taekwondo</li>
+	</ul>
+</section>
